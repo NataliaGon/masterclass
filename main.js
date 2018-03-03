65b085c9f6d3a8a078e1528f68b5b642eed04620
Natalia <gon4arovanatalija@gmail.com>
2018-03-03 13:44:37 +0200

    google map

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 32.0746012, lng: 34.7823722};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 17,
       center: uluru
     });
     var marker = new google.maps.Marker({
